8e81105c236af3028884d4ad5167c6a1df48e3ad
gabrielBorgesDornemann <gabrielbdornemann08@gmail.com>
2025-06-09 17:16:21 -0300

    Torradeira
    Dia 09/06/25
    Primeira atualização do aplicativo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import Saudacoes from "./saudacoes";
 
 export default function App() {
+  const handleButtonPress = () => {
+    Alert.alert("Botão Pressionado", "Você clicou no botão");
+  };
+
+  const handleTouchablePress = () => {
+    Alert.alert("Botão Pressionado", "Você clicou no botão personalizado");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bem-vindo ao React Native</Text>
+      <Image
+        source={{ uri: "https://reactnative.dev/img/logo-og.png" }}
+        style={styles.image}
+      />
+      <Button title="Clique Aqui" onPress={handleButtonPress} />
+      <TouchableOpacity
+        style={styles.customButton}
+        onPress={handleTouchablePress}
+      >
+        <Text style={styles.buttonText}>Botão Personalizado</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +40,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "cyan",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+  },
+
+  customButton: {
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
   },
 });
---
+++ Saudacoes.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function Saudacoes() {
+  return <Text>Saudações Trouxa</Text>;
+}
+
+export default Saudacoes;
